3c344f4b7d25b0b8b1dbe02a94559b6fe69c7177
Hasan <128808077+hasanneke@users.noreply.github.com>
2024-02-07 01:25:18 +0300

    Initial commit

---
+++ src/main/java/com/zimaberlin/givit/GivitApplication.java
@@ -0,0 +1,13 @@
+package com.zimaberlin.givit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GivitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GivitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zimaberlin/givit/GivitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zimaberlin.givit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GivitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
